c8d746a1f2602e32fe839c8df5b14fb7d782ffac
Samer Abuhaya <Samerabuhaya@gmail.com>
2025-09-01 15:14:36 +0300

    finished until spot check 3

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,53 @@
-const source = $("#wonders-template").html()
-const template = Handlebars.compile(source)
+const source = $("#wonders-template").html();
+const template = Handlebars.compile(source);
 
-const render = function(wonders){
-    $("#wonders").empty()
-    let newHtml = template({wonders})
-    $("#wonders").append(newHtml)
-}
+const render = function (wonders) {
+  $("#wonders").empty();
+  let newHtml = template({ wonders });
+  $("#wonders").append(newHtml);
+};
 
-const fetch = function(){
-    $.get("/wonders", function(response){
-        render(response)
-    })
-}
+const fetch = function () {
+  $.get("/wonders", function (response) {
+    render(response);
+  });
+};
 
-const addWonder = function(){
-    let newWonder = $("#new-wonder-input").val()
-    let newLocation = $("#new-location-input").val()
-    //POST the newWonder to the server
-}
+const addWonder = function () {
+  let newWonder = $("#new-wonder-input").val();
+  let newLocation = $("#new-location-input").val();
+  //POST the newWonder to the server
+  let data = { name: newWonder, location: newLocation };
+  $.post("/wonder", data, function (response) {
+    console.log("POST complete");
+    fetch();
+  });
+};
+const deleteWonder = function () {
+  fetch();
+};
+const updateVisited = function (wonder) {
+  $.ajax({
+    url: `wonder/${wonder}`,
+    method: "PUT",
+    success: function (response) {
+      console.log("PUT complete");
+    },
+  });
+  fetch();
+};
 
-$("#wonders").on("click", ".visit", function(){
-    let wonder = $(this).closest(".wonder").find(".name").text()
-    //PUT this to the server: update the wonder's `visited` status to `true`
-})
+$("#wonders").on("click", ".visit", function () {
+  let wonder = $(this).closest(".wonder").find(".name").text();
+  //PUT this to the server: update the wonder's `visited` status to `true`
 
+  updateVisited(wonder.split("-")[0].trim());
+});
 
-fetch() //load the data on page load
+fetch(); //load the data on page load
+
+$.ajax({
+  url: "/wonder/Colosseum",
+  method: "DELETE",
+  success: function () {},
+});
--- server.js
+++ server.js
@@ -1,23 +1,45 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
 const wonders = [
-    { name: "Mount Everest", location: "Nepal", visited: false },
-    { name: "Grand Canyon", location: "Arizona", visited: false },
-    { name: "Botanical Gardens", location: "Singapore", visited: true },
-    { name: "Pantheon", location: "Greece", visited: false },
-    { name: "Colosseum", location: "Italy", visited: true }
-]
+  { name: "Mount Everest", location: "Nepal", visited: false },
+  { name: "Grand Canyon", location: "Arizona", visited: false },
+  { name: "Botanical Gardens", location: "Singapore", visited: true },
+  { name: "Pantheon", location: "Greece", visited: false },
+  { name: "Colosseum", location: "Italy", visited: true },
+];
 
-app.get('/wonders', function (req, res) {
-    res.send(wonders)
-})
+app.get("/wonders", function (req, res) {
+  res.send(wonders);
+});
 
-const port = 1337 //because why not
+app.post("/wonder", function (req, res) {
+  console.log("Someone's trying to make a post request");
+  wonders.push({ ...req.body, visited: false });
+  res.end();
+});
+
+app.put("/wonder/:name", function (req, res) {
+  console.log("About to update someone");
+  wonders.find((w) => w.name === req.params.name).visited = true;
+  res.end();
+});
+
+app.delete("/wonder/:name", function (req, res) {
+  console.log("About to delete someone");
+  const wondersIndex = wonders.findIndex((w) => w.name === req.params.name);
+  wonders.splice(wondersIndex, 1);
+  console.log(wonders);
+  res.end();
+});
+
+const port = 1337; //because why not
 app.listen(port, function () {
-    console.log(`Server running on ${port}`)
-})
+  console.log(`Server running on ${port}`);
+});
